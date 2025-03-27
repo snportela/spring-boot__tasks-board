26869e1610a3e6be6bbbfe686e602fcddaf42ba8
Sophia Portela <sophianobreportela@gmail.com>
2025-03-26 21:45:39 -0300

    column mapper hotfix

--- src/main/java/com/example/taskBoard/column/BoardColumnMapper.java
+++ src/main/java/com/example/taskBoard/column/BoardColumnMapper.java
@@ -3,7 +3,7 @@ package com.example.taskBoard.column;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "string", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BoardColumnMapper {
 
     BoardColumnDto toDto(BoardColumn boardColumn);
